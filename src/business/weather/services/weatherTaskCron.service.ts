1313e4a4f42fef14c426f71ac27982749473b44e
zhoumeng <zhoumeng@88.com>
2021-01-17 20:44:35 +0800

    optimize

--- src/business/weather/services/weatherTaskCron.service.ts
+++ src/business/weather/services/weatherTaskCron.service.ts
@@ -1,10 +1,12 @@
 import { InjectQueue } from "@nestjs/bull";
-import { Inject, Injectable, Logger } from "@nestjs/common";
+import { CACHE_MANAGER, Inject, Injectable, Logger } from "@nestjs/common";
 import { Cron } from "@nestjs/schedule";
 import { Queue } from "bull";
+import { Cache } from "cache-manager";
 import { AreaRepository } from "../../../domain/repositories";
 import { WEAHTER_PULL_QUEUE } from './../../../infrastructure/constant';
 
+const FALG = 'flag:task:weather';
 
 @Injectable()
 export class WeatherTaskCronServcie {
@@ -12,10 +14,15 @@ export class WeatherTaskCronServcie {
     private readonly logger: Logger,
     private readonly areaRepository: AreaRepository,
     @InjectQueue(WEAHTER_PULL_QUEUE) private readonly weatherPullQueue: Queue,
+    @Inject(CACHE_MANAGER) private readonly cacheManage: Cache,
   ){}
+
 
   @Cron('0 0/30 * * * *')
   async jobHandle(){
+    const flag = await this.cacheManage.get<number>(FALG);
+    if(flag) return;
+    await this.cacheManage.set(FALG, 1, { ttl: 10 * 60, })
     this.logger.log('weatherTaskService job is executed');
     let ct = 0;
     const areaPull = this.areaRepository.selectAreaList(3);
